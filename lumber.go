63c8c7ad3826ffda4aa89e753bef39d9eedc099b
Jordan Gregory <jordangregory@granular.ag>
2019-10-03 17:03:30 -0500

    add a setlogfile method as well as a debug/fatal logger

--- lumber.go
+++ lumber.go
@@ -1,3 +1,5 @@
+// lumber is a highly optinionated logging package for Go that I seem to use in all of my packages so, it was time to make it a module
+
 package lumber
 
 import (
@@ -7,18 +9,53 @@ import (
 
 // Lumber is a struct to hold loggers
 type Lumber struct {
+	DebugLog *log.Logger
 	InfoLog  *log.Logger
 	WarnLog  *log.Logger
 	ErrorLog *log.Logger
+	FatalLog *log.Logger
 }
 
 // New returns a new instance of Lumber
+// This default behaviour will log everything to STDOUT
+//
+// Arguemnts:
+//    None
+//
+// Returns:
+//     (*Lumber): A pointer to the newly created instance of Lumber
 func New() *Lumber {
 	return &Lumber{
+		DebugLog: log.New(os.Stdout, "[DEBUG] ", log.Ldate|log.Ltime|log.Lshortfile),
 		InfoLog:  log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile),
 		WarnLog:  log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile),
 		ErrorLog: log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile),
+		FatalLog: log.New(os.Stdout, "[FATAL] ", log.Ldate|log.Ltime|log.Lshortfile),
 	}
+}
+
+// SetLogFile will set a log file for the provided logger
+//
+// Arguements:
+//     filename (string): the name of the file to write
+//     l (*log.Logger): the lumber logger to set this output file for
+//
+// Returns:
+//    (error): an error if one exists, nil otherwise
+func (L Lumber) SetLogFile(filename string, l *log.Logger) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	l.SetOutput(f)
+	return nil
+}
+
+// Debug takes a message and logs that message
+func (L Lumber) Debug(msg interface{}) {
+	L.DebugLog.Println(msg.(string))
 }
 
 // Info takes a message and logs that message
@@ -38,5 +75,5 @@ func (L Lumber) Error(msg interface{}) {
 
 // Fatal tages a message and logs that message as a fatal message
 func (L Lumber) Fatal(msg interface{}) {
-	L.ErrorLog.Fatal(msg.(string))
+	L.FatalLog.Fatal(msg.(string))
 }
